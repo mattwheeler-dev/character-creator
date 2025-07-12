6d98a19aeffa9ce42e02dfda4dabd5bea7bb6985
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2025-07-11 19:18:11 -0700

    full reset with assets saved elsewhere; start new react + vite project;

--- character-creator/src/App.jsx
+++ character-creator/src/App.jsx
@@ -1,27 +1,7 @@
-import { useState } from "react";
-import CharacterViewer from "./CharacterViewer";
-import CustomizationPanel from "./CustomizationPanel";
-
 const App = () => {
-	const [appearance, setAppearance] = useState({
-		gender: "male",
-		skin: "light",
-		hairStyle: "short",
-		hairColor: "brown",
-		facialHair: "beard",
-		facialHairColor: "brown",
-		torso: "starter_shirt",
-		legs: "starter_pants",
-		feet: "starter_boots",
-	});
 	return (
 		<>
 			<h1>Character Creator</h1>
-			<CharacterViewer appearance={appearance} />
-			<CustomizationPanel
-				appearance={appearance}
-				setAppearance={setAppearance}
-			/>
 		</>
 	);
 };
