aec7c9be39c391dd24a325127b4e2a28d539075c
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2025-07-15 21:24:24 -0700

    add 2 body types and 7 skin tones each; display selected body type with selected skin tone;

--- character-creator/src/App.jsx
+++ character-creator/src/App.jsx
@@ -1,7 +1,10 @@
+import CharacterDisplay from "./CharacterDisplay";
+
 const App = () => {
 	return (
 		<>
 			<h1>Character Creator</h1>
+			<CharacterDisplay />
 		</>
 	);
 };
---
+++ character-creator/src/CharacterDisplay.jsx
@@ -0,0 +1,97 @@
+import { useState } from "react";
+
+const bodyTypes = ["type1", "type2"];
+
+const skinTonesMap = {
+	type1: [
+		{ id: "t1_s1", value: "skin1", color: "#DFC0A4" },
+		{ id: "t1_s2", value: "skin2", color: "#FCC18D" },
+		{ id: "t1_s3", value: "skin3", color: "#FDD490" },
+		{ id: "t1_s4", value: "skin4", color: "#D09861" },
+		{ id: "t1_s5", value: "skin5", color: "#E1895F" },
+		{ id: "t1_s6", value: "skin6", color: "#624329" },
+		{ id: "t1_s7", value: "skin7", color: "#5D3A2E" },
+	],
+	type2: [
+		{ id: "t2_s1", value: "skin1", color: "#FDDBC3" },
+		{ id: "t2_s2", value: "skin2", color: "#FDD9B7" },
+		{ id: "t2_s3", value: "skin3", color: "#FDE9AF" },
+		{ id: "t2_s4", value: "skin4", color: "#F6C399" },
+		{ id: "t2_s5", value: "skin5", color: "#845242" },
+		{ id: "t2_s6", value: "skin6", color: "#6E3B2D" },
+		{ id: "t2_s7", value: "skin7", color: "#593B26" },
+	],
+};
+
+const CharacterDisplay = () => {
+	const [selectedType, setSelectedType] = useState("type1");
+	const [selectedTone, setSelectedTone] = useState("skin1");
+
+	const skinTones = skinTonesMap[selectedType];
+
+	const imagePath = `/assets/characters/${selectedType}/base/${selectedTone}.webp`;
+
+	return (
+		<section className="character-display">
+			<img
+				className="character"
+				src={imagePath}
+				alt={`Game art character with body type ${selectedType} and skin tone ${selectedTone}`}
+			/>
+
+			<div className="menu">
+				<h2>Customization Options</h2>
+				{/* Body type selection */}
+				<div className="types-btns">
+					<h3>Body Type</h3>
+					{bodyTypes.map((type) => (
+						<button
+							className={selectedType == type ? "selected" : ""}
+							key={type}
+							onClick={() => setSelectedType(type)}
+						>
+							{type === "type1" ? "Type 1" : "Type 2"}
+						</button>
+					))}
+				</div>
+
+				{/* Skin tone selection */}
+				<div className="tones-btns">
+					<h3>Skin Tone</h3>
+					{skinTones.map((tone) => (
+						<button
+							className={selectedTone == tone.value ? "selected" : ""}
+							key={tone.id}
+							onClick={() => setSelectedTone(tone.value)}
+							style={{
+								backgroundColor: tone.color,
+							}}
+						></button>
+					))}
+				</div>
+
+				{/* Hair style selection */}
+				<div className="hair-style-btns">
+					<h3>Hair Style</h3>
+				</div>
+
+				{/* Hair color selection */}
+				<div className="hair-color-btns">
+					<h3>Hair Color</h3>
+				</div>
+
+				{/* Facial hair style selection */}
+				<div className="facial-hair-style-btns">
+					<h3>Facial Hair</h3>
+				</div>
+
+				{/* Facial hair color selection */}
+				<div className="facial-hair-color-btns">
+					<h3>Facial Hair Color</h3>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default CharacterDisplay;
